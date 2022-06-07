fc3e1010b1ffe56c53098f367feb8edad5a919f4
Digiraj Solutions <digiraj.in@gmail.com>
2022-06-07 19:30:08 +0530

    Get url params with useParams function

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,9 +1,14 @@
+import { Link, useParams } from 'react-router-dom'
+
 const SingleProduct = () => {
+  const { productId } = useParams()
+
   return (
     <section className='section product'>
-      <h2>single product</h2>
+      <h2>{productId}</h2>
+      <Link to='/products'>back to products</Link>
     </section>
-  );
-};
+  )
+}
 
-export default SingleProduct;
+export default SingleProduct
