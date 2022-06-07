67a75fe58ed4f413b6da420a709abfa2e8a66551
Digiraj Solutions <digiraj.in@gmail.com>
2022-06-07 17:46:41 +0530

    add Link component to navigate within application

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom'
+
 const Home = () => {
   return (
     <section className='section'>
       <h2>Home Page</h2>
+      <Link className='btn' to='/about'>
+        Go to About
+      </Link>
     </section>
   )
 }
